f810d8ac06cb4d84710e99f39937987393cedc7a
rjsgh2389-arch <rjsgh2389@users.noreply.github.com>
2026-06-16 15:01:42 +0900

    Read server port from PORT environment variable

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('./models/Todo');
 const todoRouter = require('./routers/todos');
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const FRONTEND_ORIGIN = 'http://localhost:3000';
 const MONGO_URI = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/backend';
 
